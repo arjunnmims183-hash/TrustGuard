2b6e1bf48af202347847a3613db9552eef1fabff
Vijender <vijender4312@gmail.com>
2026-07-09 18:12:53 +0530

    changes

--- scanner/dangerous_imports.py
+++ scanner/dangerous_imports.py
@@ -46,4 +46,4 @@ class DangerousImports:
 
         scored.sort(key=lambda x: x['severity'], reverse=True)
 
-        return {{"import_analysis": {"total_imports": len(scored), "scored_imports": scored}}
+        return {"import_analysis": {"total_imports": len(scored), "scored_imports": scored}}
